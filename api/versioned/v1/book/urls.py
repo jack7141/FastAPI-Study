81fac825f967a8f9aecbaec97030c365ebf4afbf
황광회 <jack7141@nate.com>
2023-06-14 00:24:37 +0900

    git command 암기

--- api/versioned/v1/book/urls.py
+++ api/versioned/v1/book/urls.py
@@ -16,3 +16,18 @@ async def read_users():
     사용자를 생성하는 API 엔드포인트입니다.
     """
     return ["Rick", "Morty"]
+
+
+"""
+오늘 커밋하거 없어서 그냥 git 명령어 외우기,,
+git stash
+git stash list
+git stash pop
+git stash drop
+git stash apply targetname
+git restore filepath
+git restore --staged filepath
+git branch -d name
+git branch -D name
+"""
+
